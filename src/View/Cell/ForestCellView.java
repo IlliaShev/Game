7b8e889d8a379bded485e09038a24004b364b416
Vasia_Pupkin <dejnekaartem223@gmail.com>
2021-05-20 18:01:18 +0300

    Some refactor done(moved photos)

---
+++ src/View/Cell/ForestCellView.java
@@ -0,0 +1,33 @@
+package View.Cell;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+
+/**
+ * This is one of three types of cells on the board
+ * Forest cell with it's own image ready to be painted
+ *
+ * @author Vasia_Pupkin
+ * @version 1.0.0
+ * @see CellView
+ */
+public class ForestCellView extends CellView {
+
+    private static final String imageURL = "file:resources\\images\\cells\\Forest.jpg";//path to image of meadow
+
+    public ForestCellView(int length, double x, double y) {
+        super(length,x,y);
+        setCellImage();
+    }
+
+    public ForestCellView(int length) {
+        super(length);
+        setCellImage();
+    }
+
+    @Override
+    protected void setCellImage() {
+        Image mountain = new Image(imageURL);
+        this.setFill(new ImagePattern(mountain, super.getX(), super.getY(), super.getWidth(), super.getHeight(), false));
+    }
+}
